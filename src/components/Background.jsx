cdb7b95ecace6d656f2beb3ce673c1ebb3593f96
suresh <suresh12345x@gmail.com>
2025-06-22 23:25:30 +0530

    major update

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -4,8 +4,8 @@ import Forground from "./Forground";
 const Background = () => (
   <div className="w-full h-screen fixed z-0 bg-gradient-to-b from-neutral-100 to-neutral-50">
     <Forground />
-    <h1 className=" text-[5vw] leading-none tracking-tighter absolute top-1/3 left-1/2 -translate-x-[50%] -translate-x-[50%] font-semibold text-zinc-300 ">
-      Some more to goo...💥
+    <h1 className=" text-[5vw] leading-none tracking-tighter absolute top-1/2 left-1/2 -translate-x-[50%] -translate-x-[50%] font-semibold text-zinc-300 ">
+      Do
     </h1>
   </div>
 );
